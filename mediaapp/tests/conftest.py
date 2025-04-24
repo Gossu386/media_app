e74b4b20baa223eda1bc965e509aec83116110aa
Michal Goss <goss386@gmail.com>
2025-04-24 18:26:38 +0200

    added get post with comments endpoint

---
+++ mediaapp/tests/conftest.py
@@ -0,0 +1,27 @@
+from typing import AsyncGenerator, Generator
+
+import pytest
+from fastapi.testclient import TestClient
+from httpx import AsyncClient
+
+from mediaapp.main import app
+from mediaapp.routers.post import comment_table, post_table
+
+@pytest.fixture(scope="session")
+def anyio_backend():
+    return "asyncio"
+
+@pytest.fixture(autouse=True)
+def client() -> Generator:
+    yield TestClient(app)
+
+@pytest.fixture(autouse=True)
+async def db() -> AsyncGenerator:
+    post_table.clear()
+    comment_table.clear()
+    yield
+
+@pytest.fixture()
+async def async_client(client) -> AsyncGenerator:
+    async with AsyncClient(app=app, base_url=client.base_url) as ac:
+        yield ac
