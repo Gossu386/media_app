2e47cbd0a44e7dcd10846a74e5e2646c15b4aa55
Michal Goss <goss386@gmail.com>
2025-06-06 16:01:59 +0200

    ,

--- src/database.py
+++ src/database.py
@@ -9,7 +9,7 @@ post_table = sqlalchemy.Table(
     "posts",
     metadata,
     sqlalchemy.Column("id", sqlalchemy.Integer, primary_key=True),
-    sqlalchemy.Column("body", sqlalchemy.String),
+    sqlalchemy.Column("body", sqlalchemy.String)
 )
 
 comment_table = sqlalchemy.Table(
@@ -17,7 +17,7 @@ comment_table = sqlalchemy.Table(
     metadata,
     sqlalchemy.Column("id", sqlalchemy.Integer, primary_key=True),
     sqlalchemy.Column("body", sqlalchemy.String),
-    sqlalchemy.Column("post_id", sqlalchemy.ForeignKey("posts.id"), nullable=False),
+    sqlalchemy.Column("post_id", sqlalchemy.ForeignKey("posts.id"), nullable=False)
 )
 
 engine = sqlalchemy.create_engine(
