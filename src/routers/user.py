df1a30084e48521483b67a07cfee8843cbca6112
Michał <88539673+Gossu386@users.noreply.github.com>
2025-11-23 22:15:29 +0100

    9 user authentication with fastapi (#21)
    * Implement user registration and authentication features
    
    - Add user model and database table for user management
    - Create user registration and login endpoints
    - Implement password hashing and JWT token generation
    - Add tests for user registration and authentication
    - Update requirements and .gitignore for new files
    
    
    ---------
    
    Co-authored-by: Michal Goss <goss386@gmail.com>

---
+++ src/routers/user.py
@@ -0,0 +1,33 @@
+import logging
+
+from fastapi import APIRouter, HTTPException, status
+
+from src.database import database, user_table
+from src.models.user import UserIn
+from src.security import get_password_hash, get_user, authenticate_user, create_access_token
+
+logger = logging.getLogger(__name__)
+router = APIRouter()
+
+
+@router.post("/register", status_code=status.HTTP_201_CREATED)
+async def register_user(user: UserIn):
+    if await get_user(user.email):
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail="An user with that email already exists",
+        )
+
+    hashed_password = get_password_hash(user.password)
+    query = user_table.insert().values(email=user.email, password=hashed_password)
+
+    logger.debug(query)
+
+    await database.execute(query)
+    return {"detail": "User created"}
+
+@router.post("/token")
+async def login(user: UserIn):
+    user = await authenticate_user(user.email, user.password)
+    access_token = create_access_token(email=user.email)
+    return {"access_token": access_token, "token_type": "bearer"}
---
+++ src/security.py
@@ -0,0 +1,57 @@
+import logging
+import datetime
+
+from fastapi import HTTPException, status
+from jose import jwt
+from passlib.context import CryptContext
+
+from src.database import database, user_table
+
+logger = logging.getLogger(__name__)
+
+SECRET_KEY = "5456151sadsad5a1sd5asd51ac5ag56gzn"
+ALGORITHM = "HS256"
+pwd_context = CryptContext(schemes=["bcrypt"])
+
+credentials_exception = HTTPException(
+    status_code=status.HTTP_401_UNAUTHORIZED,
+    detail="Could not validate credentials"
+)
+
+def access_token_expire_minutes() -> int:
+    return 30
+
+
+def create_access_token(email: str):
+    logger.debug("Creating access token", extra={"email": email})
+    expire = datetime.datetime.now(datetime.timezone.utc) + datetime.timedelta(
+        minutes=access_token_expire_minutes()
+    )
+    jwt_data = {"sub": email, "exp": expire}
+    encoded_jwt = jwt.encode(jwt_data, SECRET_KEY, algorithm=ALGORITHM)
+    return encoded_jwt
+
+
+def get_password_hash(password: str) -> str:
+    return pwd_context.hash(password)
+
+
+def verify_password(plain_password: str, hashed_password: str) -> bool:
+    return pwd_context.verify(plain_password, hashed_password)
+
+
+async def get_user(email: str):
+    logger.debug("Fetching user from the database", extra={"email": email})
+    query = user_table.select().where(user_table.c.email == email)
+    result = await database.fetch_one(query)
+    if result:
+        return result
+    
+async def authenticate_user(email: str, password: str):
+    logger.debug("Authenticating user", extra={"email": email})
+    user = await get_user(email)
+    if not user:
+        raise credentials_exception
+    if not verify_password(password, user.password):
+        raise credentials_exception
+    return user
