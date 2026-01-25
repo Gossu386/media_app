1b7015727349eea01080b3863484683212beffe4
Michal Goss <88539673+Gossu386@users.noreply.github.com>
2026-01-25 13:08:07 +0100

    Add sorting functionality to get_all_posts endpoint and corresponding test

--- src/tests/routers/test_post.py
+++ src/tests/routers/test_post.py
@@ -160,6 +160,19 @@ async def test_get_all_posts(async_client: AsyncClient, created_post: dict):
 
 
 @pytest.mark.anyio
+async def test_get_all_posts_sorting(async_client: AsyncClient, logged_in_token: str):
+    await create_post("Test Post 1", async_client, logged_in_token)
+    await create_post("Test Post 2", async_client, logged_in_token)
+    response = await async_client.get("/post")
+    assert response.status_code == 200
+
+    data = response.json()
+    expected_order = [2, 1]
+    post_ids = [post["id"] for post in data]
+    assert post_ids == expected_order
+
+
+@pytest.mark.anyio
 async def test_create_comment(
     async_client: AsyncClient,
     created_post: dict,
